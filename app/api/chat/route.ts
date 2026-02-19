930a9608bb7f02b90889b80c398fec885ffcfe16
SIVA SANTHOSH M <146473823+sivasanthosh24@users.noreply.github.com>
2026-02-19 16:46:39 +0530

    Initial commit

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -6,11 +6,11 @@ export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
   //TODO TASK 1
-  //const systemPrompt = `You are a helpful assistant.`;
+  const systemPrompt = `You are a helpful assistant.`;
 
   const result = streamText({
     model: google('gemini-2.5-flash'),
-   // system: systemPrompt,
+    system: systemPrompt,
     messages: await convertToModelMessages(messages),
 
     //TODO TASK 2 - Tool Calling
